64b1dd71823cd91ce52aa15a5a03cbacf7cac61c
Richard Zilahi <richard.zilahi@salesframe.com>
2020-09-13 20:58:53 +0300

    file rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { theme } from './theme/theme';
+import './index.css'
+import App from './app'
+import * as serviceWorker from './serviceWorker'
+import { theme } from './theme/theme'
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -13,11 +13,10 @@ ReactDOM.render(
 			<App />
 		</ThemeProvider>
 	</React.StrictMode>,
-	document.getElementById('root')
-	);
-	
-	// If you want your app to work offline and load faster, you can change
-	// unregister() to register() below. Note this comes with some pitfalls.
-	// Learn more about service workers: https://bit.ly/CRA-PWA
-	serviceWorker.unregister();
-	
+	document.querySelector('#root'),
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
