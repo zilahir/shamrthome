3e4e404e377efaa47b26981e9d2a86dbee072231
Richard Zilahi <richard.zilahi@salesframe.com>
2020-09-18 01:47:50 +0300

    implemenitng modal

--- src/components/common/Modal/components/BounceInDown/index.tsx
+++ src/components/common/Modal/components/BounceInDown/index.tsx
@@ -4,9 +4,9 @@ import { motion } from "framer-motion";
 export const BounceInDownDiv = (props: any) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 50 }}
+      initial={{ opacity: 0, y: 0, scale: 0 }}
+      animate={{ opacity: 1, y: 0, scale: 1 }}
+      exit={{ opacity: 0, y: 0, scale: 0 }}
       {...props}
     />
   );
