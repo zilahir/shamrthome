e4b5540353b3eeb666fe43d2b26a2c7d22110a0a
Richard Zilahi <richard.zilahi@salesframe.com>
2020-09-18 13:29:30 +0300

    overlay

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,8 +1,11 @@
 import React, { ReactChild, ReactElement } from "react";
 import { AnimatePresence } from "framer-motion";
+import ReactDOM from "react-dom";
 
-import { BounceInDownDiv } from "./components/BounceInDown";
 import { colors } from "../../../theme/colors";
+
+import styles from "./Modal.module.scss";
+import { BounceInDownDiv } from "./components/BounceInDown";
 
 interface ModalProps {
   isModal: boolean;
@@ -11,39 +14,46 @@ interface ModalProps {
 }
 
 const Modal = ({ isModal, setModal, children }: ModalProps): ReactElement => {
+  const portalSelector = document.querySelector("#root");
   return (
     <AnimatePresence>
       {isModal && (
-        <BounceInDownDiv
-          style={{
-            position: "fixed",
-            background: colors.mainAppColor,
-            width: "90%",
-            height: "90%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "auto",
-            borderRadius: "5px",
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            zIndex: 9,
-          }}
-        >
-          <button
+        <>
+          <BounceInDownDiv
             style={{
-              position: "absolute",
-              top: 10,
-              right: 10,
+              position: "fixed",
+              background: colors.mainAppColor,
+              width: "90%",
+              height: "90%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "auto",
+              borderRadius: "5px",
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: 0,
+              zIndex: 9,
             }}
-            onClick={() => setModal(false)}
           >
-            X
-          </button>
-          {children}
-        </BounceInDownDiv>
+            <button
+              style={{
+                position: "absolute",
+                top: 10,
+                right: 10,
+              }}
+              onClick={() => setModal(false)}
+            >
+              X
+            </button>
+            {children}
+          </BounceInDownDiv>
+          {ReactDOM.createPortal(
+            <div className={styles.overlay} />,
+            portalSelector ? portalSelector : document.body
+          )}
+        </>
       )}
     </AnimatePresence>
   );
