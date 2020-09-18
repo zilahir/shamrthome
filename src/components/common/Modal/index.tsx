1933ed3bc565f1c8c43f7a0307539c9396d5e875
Richard Zilahi <richard.zilahi@salesframe.com>
2020-09-18 14:13:16 +0300

    modal fix.

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,9 +1,7 @@
 import React, { ReactChild, ReactElement } from "react";
 import { AnimatePresence } from "framer-motion";
 import ReactDOM from "react-dom";
-import CloseIcon from '@material-ui/icons/Close';
-
-import { colors } from "../../../theme/colors";
+import CloseIcon from "@material-ui/icons/Close";
 
 import styles from "./Modal.module.scss";
 import { BounceInDownDiv } from "./components/BounceInDown";
@@ -20,24 +18,7 @@ const Modal = ({ isModal, setModal, children }: ModalProps): ReactElement => {
     <AnimatePresence>
       {isModal && (
         <>
-          <BounceInDownDiv
-            style={{
-              position: "fixed",
-              background: colors.mainAppColor,
-              width: "90%",
-              height: "90%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              margin: "auto",
-              borderRadius: "5px",
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-              zIndex: 9,
-            }}
-          >
+          <BounceInDownDiv className={styles.modalContainer}>
             <button className={styles.closeBtn} onClick={() => setModal(false)}>
               <CloseIcon htmlColor="#ffffff" />
             </button>
